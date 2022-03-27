09d27b41db86ba9194ac1dc7a6c7d61e8225e9bc
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-03-27 08:12:29 +0500

    commit

--- src/cloneMain/SideVideos.jsx
+++ src/cloneMain/SideVideos.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SideVideos = ({ video, onVideoSelect }) => {
      return (
                <div className="section-box" onClick={() => onVideoSelect(video)} >
-                    <img src={video.snippet.thumbnails.medium.url} alt="overlay"  />
+                    <img className='section-images' src={video.snippet.thumbnails.medium.url} alt="overlay"  />
                     <div className="titleVideo">
                          <p className='header__title'>{video.snippet.title}</p>
                     </div>
